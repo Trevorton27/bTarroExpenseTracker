0fb7656ff71db42229ea974a9c61fb209310be45
Bruce Tarro <brucetarro@gmail.com>
2021-01-06 16:36:21 -0700

    edits and optimization

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # A Simple Expense Tracker App
 
- ### A simple take on an Expense Tracker
+### A simple Expense tracker that collects
 
 #### Check it out [here](https://null)
 
  <!-- ![](ADD IMAGE URL HERE) -->
 
 ## Summary
+
 <!-- WIP - App summary, future features, etc -->
 
 ## Author
+
 Bruce Tarro - Full Stack Developer [LinkedIn](https://www.linkedin.com/in/bruce-tarro/) | Personal Website - comming soon
--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-document.getElementById("submit-expense").addEventListener("click", addExpense);
+document.body.addEventListener("click", btnActions);
 
 let expenseDate;
 let expenseDescription;
 let expenseLocation;
 let expenseAmount;
+
+function btnActions(e) {
+  let btnTarget = e.target.id;
+
+  btnTarget == "submit-expense"
+    ? addExpense()
+    : btnTarget == "trash"
+    ? removeExpense(e)
+    : null;
+}
 
 function addExpense() {
   getUserInput();
@@ -23,17 +33,20 @@ function createExpense() {
   newExpense.innerHTML = `<td> ${expenseDate} </td> 
                         <td> ${expenseDescription} </td> 
                         <td> ${expenseLocation} </td> 
-                        <td class=money>$ ${expenseAmount} </td>`;
+                        <td class=money>$ ${expenseAmount} </td>
+                        <td><img id="trash" src="images/new_trash.svg" /></td>`;
   document.getElementById("tracker-body").appendChild(newExpense);
 }
 
 function resetUserInput() {
-  document.getElementById("expense-date").value = "";
-  document.getElementById("expense-description").value = "";
-  document.getElementById("expense-location").value = "";
-  document.getElementById("expense-amount").value = "";
+  let userInput = document.querySelectorAll(".btn");
+  let eachBtn;
+  for (eachBtn = 0; eachBtn < userInput.length - 1; eachBtn++) {
+    userInput[eachBtn].value = "";
+  }
 }
 
-// TODO
-
-// Delete function
+function removeExpense(e) {
+  let targetExpense = e.target.id;
+  document.getElementById(targetExpense).parentNode.parentNode.remove();
+}
