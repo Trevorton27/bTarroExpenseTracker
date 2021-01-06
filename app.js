ae82d910a60351e39b75f2b18fb89be739bbdf09
Bruce Tarro <brucetarro@gmail.com>
2021-01-05 23:08:51 -0700

    Base logic and styling;

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log("Is this thing on?");
+document.getElementById("submit-expense").addEventListener("click", addExpense);
+
+let expenseDate;
+let expenseDescription;
+let expenseLocation;
+let expenseAmount;
+
+function addExpense() {
+  getUserInput();
+  createExpense();
+  resetUserInput();
+}
+
+function getUserInput() {
+  expenseDate = document.getElementById("expense-date").value;
+  expenseDescription = document.getElementById("expense-description").value;
+  expenseLocation = document.getElementById("expense-location").value;
+  expenseAmount = document.getElementById("expense-amount").value;
+}
+
+function createExpense() {
+  let newExpense = document.createElement("tr");
+  newExpense.innerHTML = `<td> ${expenseDate} </td> 
+                        <td> ${expenseDescription} </td> 
+                        <td> ${expenseLocation} </td> 
+                        <td class=money>$ ${expenseAmount} </td>`;
+  document.getElementById("tracker-body").appendChild(newExpense);
+}
+
+function resetUserInput() {
+  document.getElementById("expense-date").value = "";
+  document.getElementById("expense-description").value = "";
+  document.getElementById("expense-location").value = "";
+  document.getElementById("expense-amount").value = "";
+}
+
+// TODO
+
+// Delete function
